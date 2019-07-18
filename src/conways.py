5887136491d72d0e74bb73197e26e455fba52c9c
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-18 12:48:09 +0100

    Add code from lecture

--- src/conways.py
+++ src/conways.py
@@ -4,20 +4,26 @@ import pygame, random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (25, 25, 25)
-RED = (255, 0, 0)
+BTN_COLOR = (175, 203, 255)
 MARGIN = 3
 SQ_LENGTH = 20
 SQ_NUM = 25
 WIN_SIZE = (SQ_NUM + 1) * MARGIN + SQ_NUM * SQ_LENGTH
+BTN_SIZE = 30
 
 pygame.init()
 
 # Set the width and height of the screen [width, height]
-size = (WIN_SIZE, WIN_SIZE)
+size = (WIN_SIZE, WIN_SIZE + BTN_SIZE + 20)
 screen = pygame.display.set_mode(size)
 
 # Initial state
 automata = [0] * (SQ_NUM * SQ_NUM)
+
+# TODO add some variables to track generations and speed of game start and stop etc
+generations = 0
+time_step = 5
+running = True
 
 # Assign Random Values to our Automata
 # rowx:
@@ -34,11 +40,43 @@ for row in range(SQ_NUM):
         automata[row * SQ_NUM + col] = random.randint(0, 1)
 
 
+# TODO add some special figures on to the screen
+
+# Block
+automata[3] = 1
+automata[4] = 1
+automata[SQ_NUM + 3] = 1
+automata[SQ_NUM + 4] = 1
+
+# Bee-Hive
+
+automata[SQ_NUM - 6] = 1
+automata[SQ_NUM - 5] = 1
+automata[SQ_NUM + SQ_NUM - 4] = 1
+automata[SQ_NUM + SQ_NUM - 7] = 1
+automata[2 * SQ_NUM + SQ_NUM - 5] = 1
+automata[2 * SQ_NUM + SQ_NUM - 6] = 1
+
+# Blinker
+# Beacon
+# Glider
+
+
+
+
 # Add a title
 pygame.display.set_caption("Conway's Game of Life")
 
 # Loop until the user clicks the close button.
 done = False
+
+# TODO add a font
+
+font = pygame.font.Font('freesansbold.ttf', 16)
+
+# TODO Add a button
+
+inc_timestep_button = pygame.draw.rect(screen, BTN_COLOR, pygame.Rect(10, WIN_SIZE + 10, 3 * BTN_SIZE, BTN_SIZE))
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
@@ -49,6 +87,15 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+    #     Click event
+    #     TODO this is where all events are added
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            click_position = pygame.mouse.get_pos()
+
+            # use the pos of mouse to decide which button was pressed
+            if inc_timestep_button.collidepoint(click_position) and time_step < 20:
+                print("faster")
+                time_step += 1
 
     # --- Game logic should go here
 
@@ -132,12 +179,21 @@ while not done:
             i += 1
             x += SQ_LENGTH + MARGIN
         y += SQ_LENGTH + MARGIN
+    # Update inc timestep button
+    inc_timestep_button = pygame.draw.rect(screen, BTN_COLOR, pygame.Rect(10, WIN_SIZE + 10, 3 * BTN_SIZE, BTN_SIZE))
+    text = font.render("Increment", True, (14, 28, 54))  # Change text in button and refactor colour
+    text_rect = text.get_rect()
+    text_rect.center = (inc_timestep_button.center[0], inc_timestep_button.center[1])
+    screen.blit(text, text_rect)
+
+
+    # TODO add other button updates
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
     # --- Limit to 5 frames per second
-    clock.tick(5)
+    clock.tick(time_step)
 
 # Close the window and quit.
 pygame.quit()
